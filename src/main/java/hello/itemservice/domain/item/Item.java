e8ceaabc305f265acc219b0ff31ae9c9d247816b
ted7088 <ted3131@naver.com>
2024-03-03 15:38:24 +0900

    commit

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -9,20 +9,19 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >=10000", message = "총합이 만원 넘게 입력해주세요")
 public class Item {
-    @NotNull (groups = UpdateCheck.class)
+//    @NotNull (groups = UpdateCheck.class)
     private Long id;
 
-    @NotBlank (groups ={SaveCheck.class, UpdateCheck.class})
+ //   @NotBlank (groups ={SaveCheck.class, UpdateCheck.class})
     private String itemName;
 
-    @NotNull (groups ={SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000)
+  //  @NotNull (groups ={SaveCheck.class, UpdateCheck.class})
+    //@Range(min = 1000, max = 1000000)
     private Integer price;
 
-    @NotNull (groups ={SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class})
+   // @NotNull (groups ={SaveCheck.class, UpdateCheck.class})
+   // @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
